703e477f88030f8272245c3e336d9ae578bee8aa
Shadabdullah <shadabdullah101@gmail.com>
2024-01-12 21:21:59 +0530

    UI Ready

--- herokucicd/app/page.tsx
+++ herokucicd/app/page.tsx
@@ -1,13 +1,62 @@
 "use client";
-import { useState } from "react";
+import { Scope_One } from "next/font/google";
+import { useState, useEffect } from "react";
 
 export default function Home() {
-  const [hour, setHour] = useState<number | string>("00");
-  const [minute, setMinutes] = useState<number | string>("00");
-  const [seconds, setSeconds] = useState<number | string>("00");
+  const [hour, setHour] = useState<number>(0);
+  const [minute, setMinutes] = useState<number>(0);
+  const [seconds, setSeconds] = useState<number>(0);
   const [breaks, setBreaks] = useState<number>(5);
+  const [timeInMin, setTimeInMin] = useState<number>(0);
 
-  const timeHandler = () => {};
+  
+
+  function convertMinutesToTime(minutes: number): {
+    hours: number;
+    minute: number;
+    seconds: number;
+  } {
+    let hours = Math.floor(minutes / 60);
+    const remainingMinutes = minutes % 60;
+    let seconds = 0;
+    let minute = Math.floor(remainingMinutes);
+    if (minute >= 1) {
+      seconds = 60;
+      minute -= 1;
+    } else if (minute == 0 && hours >= 1) {
+      hours -= 1;
+      minute = 59;
+      seconds = 60;
+    }
+
+    return {
+      hours,
+      minute,
+      seconds,
+    };
+  }
+
+  const timeHandler = () => {
+    const { hours, minute, seconds } = convertMinutesToTime(timeInMin);
+    setHour(hours);
+    setMinutes(minute);
+    setSeconds(seconds);
+  };
+
+  const timeSetter = (event: React.ChangeEvent<HTMLInputElement>) => {
+    const newValue = parseInt(event.target.value);
+    setTimeInMin(isNaN(newValue) ? 0 : newValue);
+  };
+
+  const breakHandler = (event: React.ChangeEvent<HTMLInputElement>) => {
+    setBreaks(
+      isNaN(parseInt(event.target.value)) ? 0 : parseInt(event.target.value)
+    );
+  };
+
+  const timerHandler = () => {
+
+  };
 
   return (
     <div className="flex justify-center items-center h-screen font-mono text-lg">
@@ -19,12 +68,14 @@ export default function Home() {
             className=" text-center text-black rounded-sm border border-red-500 mr-2"
             placeholder="Minutes"
             type="number"
+            onChange={timeSetter}
           />
           <input
             id="break"
             className=" text-center text-black rounded-sm mr-2"
             placeholder="Break"
             type="number"
+            onChange={breakHandler}
           />
           <button
             className="bg-sky-700 h-7 w-24 rounded-sm"
@@ -32,21 +83,27 @@ export default function Home() {
           >
             OK
           </button>
+          <button
+            className=" h-7 w-24 rounded-sm bg-green-400 ml-4"
+            onClick={timerHandler}
+          >
+            Start
+          </button>
         </div>
         <div className="flex-col justify-center  items-center flex space-y-4 border  w-1/2 h-52  border-red-100 text-4xl">
           <div className=" flex justify-between p-4 w-60">
             {" "}
-            <span>H</span>
-            <span>M</span>
-            <span>S</span>
-            <span>B</span>
+            <span className="text-red-500 ml-3">H</span>
+            <span className=" text-blue-700 ml-4">M</span>
+            <span className=" text-blue-700 ml-4">S</span>
+            <span className=" text-yellow-300 ml-6">B</span>
           </div>
 
           <div className=" flex justify-between p-4 w-60">
-            <span>{hour}:</span>
-            <span>{minute}:</span>
-            <span>{seconds}:</span>
-            <span>{breaks}</span>
+            <span className="text-red-500">{hour}:</span>
+            <span className="text-blue-700">{minute}:</span>
+            <span className=" text-blue-700">{seconds}</span>
+            <span className="text-yellow-300 ml-6">{breaks}</span>
           </div>
         </div>
       </div>
